ecad704353967b6da68420d94abe926f99017e3c
Jesper Skrufve <jesper@neolo.gy>
2012-03-21 06:52:09 +0100

    Initial Commit

---
+++ NLKitExample/NLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NLAppDelegate.h
+//  NLKitExample
+//
+//  Created by Jesper Skrufve on 21/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NLKitExampleTests/NLKitExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  NLKitExampleTests.h
+//  NLKitExampleTests
+//
+//  Created by Jesper Skrufve on 21/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NLKitExampleTests : SenTestCase
+
+@end
