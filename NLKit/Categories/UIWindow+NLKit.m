381fa8e788bf61957d8e1f80162adc824c1f8730
Jesper Skrufve <jesper.skrufve@gmail.com>
2013-01-17 14:49:33 +0200

    made sure correct window is returned

--- NLKit/Categories/UIWindow+NLKit.m
+++ NLKit/Categories/UIWindow+NLKit.m
@@ -2,17 +2,17 @@
 //  UIWindow+NLKit.m
 //
 //  Created by Jesper Skrufve <jesper@neolo.gy>
-//  
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
 //  deal in the Software without restriction, including without limitation the
 //  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 //  sell copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be included in
 //  all copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //  IN THE SOFTWARE.
-//  
+//
 
 #import "UIWindow+NLKit.h"
 
@@ -29,12 +29,12 @@
 + (UIWindow *)mainWindow
 {
 	UIWindow* window = [[UIApplication sharedApplication] keyWindow];
-	
-	if (window)
+
+	if (window && ![window isKindOfClass:[NSClassFromString(@"_UIAlertNormalizingOverlayWindow") class]])
 		return window;
-	
+
 	NSArray* windows = [[UIApplication sharedApplication] windows];
-	
+
 	return [windows count] ? windows[0] : nil;
 }
 
@@ -44,7 +44,7 @@
 		for (UIView* view in [window subviews])
 			if ([view isKindOfClass:NSClassFromString(@"UIPeripheralHostView")])
 				return view;
-	
+
 	return nil;
 }
 
