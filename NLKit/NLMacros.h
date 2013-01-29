67a2facd007fa5124d6c933c58ef0b5ef6892457
Jesper Skrufve <jesper.skrufve@gmail.com>
2013-01-29 11:53:03 +0200

    changed erronous keyboard height

--- NLKit/NLMacros.h
+++ NLKit/NLMacros.h
@@ -2,17 +2,17 @@
 //  NLMacros.h
 //
 //  Created by Jesper Skrufve <jesper@neolo.gy>
-//  
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
 //  deal in the Software without restriction, including without limitation the
 //  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 //  sell copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be included in
 //  all copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //  IN THE SOFTWARE.
-//  
+//
 
 #ifndef NLKit_NLMacros_h
 #define NLKit_NLMacros_h
@@ -47,7 +47,7 @@
 #define NSStringFromNSIndexPath(ip)				[NSString stringWithFormat:@"{%i, %i}", ip.section, ip.row]
 
 #define ARC4RANDOM_MAX							UINT32_MAX
-#define IPHONE_KEYBOARD_HEIGHT					166.f
+#define IPHONE_KEYBOARD_HEIGHT					216.f
 #define KEYBOARD_ANIMATION_DURATION				0.3f
 
 #define _radians(degrees)						(M_PI * (degrees) / 180.0)
